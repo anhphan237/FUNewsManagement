18f7939cc94bf27b96ac55395ad5743f1adabee6
anhphan237 <anh713988@gmail.com>
2025-02-13 23:45:02 +0700

    Download Pakage for DB Connection

--- FUNewsManagementASPNETCoreMVC/DataAccessObjects/obj/Debug/net8.0/DataAccessObjects.AssemblyInfo.cs
+++ FUNewsManagementASPNETCoreMVC/DataAccessObjects/obj/Debug/net8.0/DataAccessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73d7e75b254c3543e18f4de441f1b701a8782b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81873af5ed19693acff93d5f1dc41fc84d43dbf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
