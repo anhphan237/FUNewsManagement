4ebcac403927592cf4f0e2fe89b8ea187194bca9
anhphan237 <anh713988@gmail.com>
2025-02-17 02:30:45 +0700

    Updated

--- FUNewsManagementASPNETCoreMVC/FUNewsManagement/obj/Debug/net8.0/FUNewsManagement.AssemblyInfo.cs
+++ FUNewsManagementASPNETCoreMVC/FUNewsManagement/obj/Debug/net8.0/FUNewsManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUNewsManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15b7b057ca32a8a3e5410766d4bc2d0ca9dea09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d422a0fadf347da3e90a6e064a431f72af6c9015")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUNewsManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUNewsManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
