816205c508b00f5839f93cb4ac523ce1a97c4e09
anhphan237 <anh713988@gmail.com>
2025-02-18 06:18:59 +0700

    Updated Account List

--- FUNewsManagementASPNETCoreMVC/FUNewsManagement/obj/Debug/net8.0/FUNewsManagement.AssemblyInfo.cs
+++ FUNewsManagementASPNETCoreMVC/FUNewsManagement/obj/Debug/net8.0/FUNewsManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUNewsManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52733f32fe8ce76c8da4cb82f5a5457bb8af29c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+569fc4a7163fe429c07931e91e65766fd4a55aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUNewsManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUNewsManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
